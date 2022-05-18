11fe205e593868bcb6ce3672bdcabdc5ca0e3f04
Nightstars <ChristZhangowner@gmail..com>
2022-05-18 14:15:11 +0800

    new features

--- SmartTools/ViewModels/MainWindowViewModel.cs
+++ SmartTools/ViewModels/MainWindowViewModel.cs
@@ -21,7 +21,7 @@ namespace SmartToolsDotNet.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         #region initialize
-        private const string baseUrl = @"http://127.0.0.1:5000", hubName = "versionhub";
+        private const string baseUrl = @"http://127.0.0.1:5004/api/AppInfo", hubName = "versionhub";
         private ClientParameter clientParameter;
         public MainWindowViewModel(IRegionManager regionManager)
         {
